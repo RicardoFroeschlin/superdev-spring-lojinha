6998e7d3b73f858bd2ca9741292f3e38babe28ae
RicardoFroeschlin <ricardo.m.souza3332@gmail.com>
2024-05-17 22:26:19 -0300

    :sparkles: feat(produto): CREATE and READ para Produto

--- lojinha/src/main/java/com/superdev/lojinha/LojinhaApplication.java
+++ lojinha/src/main/java/com/superdev/lojinha/LojinhaApplication.java
@@ -2,6 +2,7 @@ package com.superdev.lojinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class LojinhaApplication {
